5881a8ef160a622d574b0e510af013e2fb8fd214
David Greaves <david@dgreaves.com>
2014-01-02 14:20:10 +0000

    Add support for accessing the last screenshot taken and for deleting it
    Signed-off-by: David Greaves <david@dgreaves.com>

--- src/shot.cpp
+++ src/shot.cpp
@@ -1,24 +1,60 @@
 #include "shot.h"
 #include <QDebug>
+#include <QStringBuilder>
 
-Shot::Shot(QObject *parent) : QObject(parent)
+Shot::Shot(QObject *parent) : QObject(parent),
+    m_left(0),
+    m_timer(new QTimer(this)),
+    m_picDir("/home/nemo/Pictures"),
+    m_lastShotDT(),
+    m_lastShot("")
 {
-    m_timer = new QTimer(this);
+    if (!QStandardPaths::standardLocations(QStandardPaths::PicturesLocation).isEmpty()) {
+        m_picDir=QStandardPaths::standardLocations(QStandardPaths::PicturesLocation).first();
+        qDebug()<< "Found pictures at " << m_picDir;
+    } else {
+        qDebug()<< "Fallback to pictures at " << m_picDir;
+    }
     m_timer->setInterval(1000);
     connect(m_timer, SIGNAL(timeout()), this, SLOT(Update()));
-    m_left = 0;
 }
 
 void Shot::ShootNow() {
     emit shooting();
+    m_lastShotDT=QDateTime::currentDateTime();
+    QDBusMessage reply;
     QDBusConnection bus = QDBusConnection::sessionBus();
     QDBusInterface dbus_iface("org.nemomobile.lipstick", "/org/nemomobile/lipstick/screenshot",
                               "org.nemomobile.lipstick", bus);
-    qDebug() << dbus_iface.call("saveScreenshot","");
+    reply = dbus_iface.call("saveScreenshot","");
+    qDebug() << reply;
     QTimer::singleShot(200, this, SLOT(emitShotDone()));
 }
+
+// This routine calls itself using a timer until the shot appears.
 void Shot::emitShotDone() {
-    emit shotDone();
+    QString file;
+    QDateTime trial = m_lastShotDT;
+    // There's a chance the screenshot takes a while...
+    int t=0;
+    do {
+        m_lastShot=m_picDir % "/" % trial.toString("yyyyMMddhhmmss") % ".png";
+        if (QFile(m_lastShot).exists()) {
+            emit lastShotChanged(m_lastShot);
+            emit shotDone();
+            qDebug()<< "Found a pic " << m_lastShot;
+            return;
+        }
+        trial = trial.addSecs(1);
+        t++;
+        qDebug()<< "Looking for a pic " << t;
+    } while (t<5);
+
+    // keep trying for 5s
+    if (m_lastShotDT.secsTo(QDateTime::currentDateTime()) < 5)
+        QTimer::singleShot(200, this, SLOT(emitShotDone()));
+
+    m_lastShot="";
 }
 
 void Shot::Cancel() {
@@ -27,6 +63,7 @@ void Shot::Cancel() {
     emit leftChanged(m_left);
     emit activeChanged(active());
 }
+
 void Shot::Shoot(int delay) {
     if (delay <= 0) {
         m_timer->stop();
@@ -50,12 +87,21 @@ void Shot::Update() {
         emit activeChanged(active());
     }
 }
+void Shot::deleteCurrent() {
+    QFile curr(m_lastShot);
+    if ( curr.exists() ) {
+        curr.remove();
+        m_lastShot="";
+        emit lastShotChanged(m_lastShot);
+    }
+}
+
+QString Shot::lastShot() {
+    return m_lastShot;
+}
 
 bool Shot::active() {
     return m_timer->isActive();
-}
-
-void Shot::setActive(bool a) {
 }
 
 int Shot::left() {
--- src/shot.h
+++ src/shot.h
@@ -7,6 +7,7 @@
 #include <QtCore/QStringList>
 #include <QtDBus/QtDBus>
 #include <QTimer>
+#include <QStandardPaths>
 
 class Shot : public QObject
 {
@@ -14,13 +15,14 @@ class Shot : public QObject
 public:
     explicit Shot(QObject *parent = 0);
 
-    Q_PROPERTY(bool active READ active WRITE setActive NOTIFY activeChanged)
+    Q_PROPERTY(bool active READ active NOTIFY activeChanged)
     Q_PROPERTY(int left READ left WRITE setLeft NOTIFY leftChanged)
+    Q_PROPERTY(QString lastShot READ lastShot NOTIFY lastShotChanged)
 
     bool active();
-    void setActive(bool a);
     int left();
     void setLeft(int l);
+    QString lastShot();
 
     Q_INVOKABLE void Shoot(int d);
     Q_INVOKABLE void Cancel();
@@ -29,17 +31,23 @@ public slots:
     void ShootNow();
     void Update();
     void emitShotDone();
+    void deleteCurrent();
 
 signals:
     void activeChanged(bool);
     void leftChanged(int);
     void shooting();
     void shotDone();
+    void lastShotChanged(QString);
 
 private:
     bool m_active;
     int m_left;
     QTimer* m_timer;
+    QString m_picDir;
+    QDateTime m_lastShotDT; // Time shot taken
+    QString m_lastShot;    // file found near time taken
+                            // null = not checked / "" not found / "val"
 };
 
 #endif // SHOT_H
